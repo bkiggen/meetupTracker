375124b82d09188b28fc9f0d738367174f6e0515
Ben Kiggen <benkiggen@gmail.com>
2020-03-15 11:46:32 -0700

    log user in

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -18,6 +18,52 @@ router.get("/", auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
-})
+});
+
+router.post('/', [
+  check('email', 'Please use a valid email.').isEmail(),
+  check('password', 'Password is required.').exists()
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ errors: [ {msg: 'Invalid credentials!'}] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{ msg: 'Invalid credentials!' }]
+        });
+      }
+      
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000},
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      )
+    } catch (err){
+
+    }
+  });
 
 module.exports = router;
